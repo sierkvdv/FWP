e2b65527e7f21ba53edee82b2c952dbf91047bfb
sierkvdv <sierkvd@gmail.com>
2025-10-23 11:50:50 +0200

    Update contact email to fieldworksproduction@gmail.com

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,9 +1,9 @@
 import { ContactInfo } from '../types';
 
 export const contactInfo: ContactInfo = {
-  email: 'info@fwp.one', // Vervang dit met je echte email
-  linkedin: 'https://www.linkedin.com/in/FWP-one/', // Vervang met je LinkedIn
-  github: 'https://github.com/sierkvdv', // Dit klopt al
-  instagram: 'https://www.instagram.com/sierkvdv?igsh=MXg5ZmhoMm9hbG96Mg%3D%3D&utm_source=qr'  // Verwijder als je geen Instagram hebt
+  email: 'fieldworksproduction@gmail.com',
+  linkedin: 'https://www.linkedin.com/in/FWP-one/',
+  github: 'https://github.com/sierkvdv',
+  instagram: 'https://www.instagram.com/sierkvdv?igsh=MXg5ZmhoMm9hbG96Mg%3D%3D&utm_source=qr'
 };
 // Test push after deleting sierkvd account 
